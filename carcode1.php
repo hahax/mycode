635c0e98f450dc51c29ba16b1df500bcc1421ea0
zl <75506525@qq.com>
2018-03-22 10:26:15 +0800

    t3

--- carcode1.php
+++ carcode1.php
@@ -330,6 +330,62 @@ class carcode1{
             }
         }
     }
+
+    /**
+     * desription 压缩图片
+     * @param sting $imgsrc 图片路径
+     * @param string $imgdst 压缩后保存路径
+     * $w 图片宽
+     * $h 图片高度
+     */
+    function image_png_size_add($imgsrc, $imgdst,$w,$h)
+    {
+        list($width, $height, $type) = getimagesize($imgsrc);
+        $new_width = $width>600?$w:$width;
+        $new_height = $height>600?$h:$height;
+        switch ($type) {
+            case 1:
+                $giftype = check_gifcartoon($imgsrc);
+                if ($giftype) {
+                    header('Content-Type:image/gif');
+                    $image_wp = imagecreatetruecolor($new_width, $new_height);
+                    $image = imagecreatefromgif($imgsrc);
+                    imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                    imagejpeg($image_wp, $imgdst, 75);
+                    imagedestroy($image_wp);
+                }
+                break;
+            case 2:
+                header('Content-Type:image/jpeg');
+                $image_wp = imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefromjpeg($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                imagejpeg($image_wp, $imgdst, 75);
+                imagedestroy($image_wp);
+                break;
+            case 3:
+                header('Content-Type:image/png');
+                $image_wp = imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefrompng($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                imagejpeg($image_wp, $imgdst, 75);
+                imagedestroy($image_wp);
+                break;
+        }
+    }
+
+    /**
+     * desription 判断是否gif动画
+     * @param sting $image_file图片路径
+     * @return boolean t 是 f 否
+     */
+    function check_gifcartoon($image_file)
+    {
+        $fp = fopen($image_file, 'rb');
+        $image_head = fread($fp, 1024);
+        fclose($fp);
+        return preg_match("/" . chr(0x21) . chr(0xff) . chr(0xb) . 'NETSCAPE2.0' . "/", $image_head) ? false : true;
+    }
 }
 
 
